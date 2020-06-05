36c9f6e5e68a670271719740685a8d8c96d09b73
Jabir MT <jabirmt7824@gmail.com>
2020-06-05 21:40:23 +0530

    page rendered

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const port = 2001;
+const port = 1001;
 
 const app = express();
 
+app.use(express.static("./assets"));
+
+app.use("/", require("./routes/index"));
+
+app.set("view engine", "ejs");
+app.set("views","./views");
 
 app.listen(port, function(err){
     if(err)
